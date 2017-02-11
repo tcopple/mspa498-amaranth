f991f4e852931b647500d022cda7f537637496f8
Tyler Copple <j.tylercopple@gmail.com>
2017-02-11 16:15:06 -0500

    Updates to svytable eda to produce flusht by demographic contingency tables.

--- src/r/020-eda-svytable.r
+++ src/r/020-eda-svytable.r
@@ -23,9 +23,47 @@ monet.db = dbConnect(MonetDBLite::MonetDBLite(), path.monet)
 brfss.2014 = open(brfss.2014.design, driver=MonetDB.R())
 brfss.2015 = open(brfss.2015.design, driver=MonetDB.R())
 
-variable.state = as.formula("~xstate")
-variable.flshot = as.formula("~flushot6")
+rda.path = file.path("demographic-by-response-tables")
+dir.create(rda.path)
 
-options(survey.lonely.psu="adjust")
-table.2014 = svytable(~factor(xstate)+factor(flushot6), brfss.2014)
-table.2015 = svytable(~factor(xstate)+factor(flushot6), brfss.2015)
+employ.by.flu.2014 = func.two.level.table(brfss.2014, "employ1", "flushot6", ~factor(employ1) + factor(flushot6), as.percent = TRUE)
+save(employ.by.flu.2014, file = file.path(rda.path, "employ-by-flu-2014.rda"))
+
+employ.by.flu.2015 = func.two.level.table(brfss.2015, "employ1", "flushot6", ~factor(employ1) + factor(flushot6), as.percent = TRUE)
+save(employ.by.flu.2015, file = file.path(rda.path, "employ-by-flu-2015.rda"))
+
+educa.by.flu.2014 = func.two.level.table(brfss.2014, "educa", "flushot6", ~factor(educa) + factor(flushot6), as.percent = TRUE)
+save(educa.by.flu.2014, file = file.path(rda.path, "educa-by-flu-2014.rda"))
+
+educa.by.flu.2015 = func.two.level.table(brfss.2015, "educa", "flushot6", ~factor(educa) + factor(flushot6), as.percent = TRUE)
+save(educa.by.flu.2015, file = file.path(rda.path, "educa-by-flu-2015.rda"))
+
+income2.by.flu.2014 = func.two.level.table(brfss.2014, "income2", "flushot6", ~factor(income2) + factor(flushot6), as.percent = TRUE)
+save(income2.by.flu.2014, file = file.path(rda.path, "income2-by-flu-2014.rda"))
+
+income2.by.flu.2015 = func.two.level.table(brfss.2015, "income2", "flushot6", ~factor(income2) + factor(flushot6), as.percent = TRUE)
+save(income2.by.flu.2015, file = file.path(rda.path, "income2-by-flu-2015.rda"))
+
+marital.by.flu.2014 = func.two.level.table(brfss.2014, "marital", "flushot6", ~factor(marital) + factor(flushot6), as.percent = TRUE)
+save(marital.by.flu.2014, file = file.path(rda.path, "marital-by-flu-2014.rda"))
+
+marital.by.flu.2015 = func.two.level.table(brfss.2015, "marital", "flushot6", ~factor(marital) + factor(flushot6), as.percent = TRUE)
+save(marital.by.flu.2015, file = file.path(rda.path, "marital-by-flu-2015.rda"))
+
+renthom1.by.flu.2014 = func.two.level.table(brfss.2014, "renthom1", "flushot6", ~factor(renthom1) + factor(flushot6), as.percent = TRUE)
+save(renthom1.by.flu.2014, file = file.path(rda.path, "renthom1-by-flu-2014.rda"))
+
+renthom1.by.flu.2015 = func.two.level.table(brfss.2015, "renthom1", "flushot6", ~factor(renthom1) + factor(flushot6), as.percent = TRUE)
+save(renthom1.by.flu.2015, file = file.path(rda.path, "renthom1-by-flu-2015.rda"))
+
+sex.by.flu.2014 = func.two.level.table(brfss.2014, "sex", "flushot6", ~factor(sex) + factor(flushot6), as.percent = TRUE)
+save(sex.by.flu.2014, file = file.path(rda.path, "sex-by-flu-2014.rda"))
+
+sex.by.flu.2015 = func.two.level.table(brfss.2015, "sex", "flushot6", ~factor(sex) + factor(flushot6), as.percent = TRUE)
+save(sex.by.flu.2015, file = file.path(rda.path, "sex-by-flu-2015.rda"))
+
+internet.by.flu.2014 = func.two.level.table(brfss.2014, "internet", "flushot6", ~factor(internet) + factor(flushot6), as.percent = TRUE)
+save(internet.by.flu.2014, file = file.path(rda.path, "internet-by-flu-2014.rda"))
+
+internet.by.flu.2015 = func.two.level.table(brfss.2015, "internet", "flushot6", ~factor(internet) + factor(flushot6), as.percent = TRUE)
+save(internet.by.flu.2015, file = file.path(rda.path, "internet-by-flu-2015.rda"))
--- src/r/helpers.R
+++ src/r/helpers.R
@@ -23,3 +23,31 @@ func.getDemographicVars = function() {
 func.getGeographicVars = function() {
   return(filter(read.csv("variable-details.csv", header = TRUE, sep = ","), Geographical == 1)$Field.Name)
 }
+
+func.two.level.table = function(study, var.x, var.y, formula, as.percent = FALSE){
+  results = data.frame()
+  if(as.percent) {
+    results = as.data.frame(svytable(formula, study, Ntotal=100))
+  } else {
+    results = as.data.frame(svytable(formula, study))
+  }
+
+  colnames(results) = c(var.x, var.y, "freq")
+  results = func.replace.codes(results, var.x)
+  results = func.replace.codes(results, var.y)
+  return(results[order(-results$freq),])
+}
+
+func.replace.codes = function(df, var.name) {
+  codebook_filepath = paste0("../../data/supplemental/", year.2015, "datadict.json")
+  codebooks = fromJSON(codebook_filepath)
+
+  responses = codebooks[var.name]
+  codes = stack(responses[[1]]$codes)
+  colnames(codes) = c("value", "mnemonic")
+
+  result = merge(df, codes, by.x=var.name, by.y="mnemonic")
+  result[, var.name] = result$value
+  result$value = NULL
+  return(result)
+}
